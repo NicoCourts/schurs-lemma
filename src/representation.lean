88417638817e7fcd097cebefa583571b4181ba82
Nico Courts <nico@nicocourts.com>
2020-06-19 16:04:03 -0700

    Preliminary definition?

--- src/representation.lean
+++ src/representation.lean
@@ -1,8 +1,16 @@
 import group_theory.submonoid
 import algebra.module
 
-variables {G : Type*} [group G]
-variables {𝕜 : Type*} [field 𝕜]
-variables {M : Type*} [module 𝕜 M]
+universe u
+variables {G : Type u} [group G] {𝕜 : Type u} [field 𝕜] {M : Type u} [add_comm_group M] [module 𝕜 M]
 
-structure representation (G : Type*) [group G] extends (module 𝕜 M)
+-- From https://github.com/fpvandoorn/group-representations/blob/master/src/group_theory/representation/basic.lean
+class G_module (G : Type*) [group G] (M : Type*) [add_comm_group M]
+  extends has_scalar G M :=
+(id : ∀ m : M, (1 : G) • m = m)
+(mul : ∀ g h : G, ∀ m : M, g • (h • m) = (g * h) • m)
+(linear : ∀ g : G, ∀ m n : M, g • (m + n) = g • m + g • n)
+
+-- A G-module whose action is 𝕜-linear
+structure representation [h: has_scalar 𝕜 M] extends G_module G M :=
+(ex : ∀ k : 𝕜, ∀ m : M, ∀ g : G,  g • (k • m) = k • (g • m))
